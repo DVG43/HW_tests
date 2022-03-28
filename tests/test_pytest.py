dac6fe0d3ede871e6af6948de82f4f9a83ee2471
Galuta Dmitrii <dvgaluta@gmail.com>
2022-03-29 00:36:51 +0300

    commit_3

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,12 @@
 import pytest
-from main import returning_name, returning_directories,returning_lis
+from main import returning_name, returning_directories, returning_lis
+
+documents = [
+    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+]
+
 
 class TestFunctionsPytest:
     def setup(self):
@@ -9,10 +16,10 @@ class TestFunctionsPytest:
         print('teardown')
 
     def test_returning_name(self):
-        assert returning_name () ==
+        assert returning_name("11-2", documents) == "Геннадий Покемонов"
 
-    def test_returning_directories(self):
-        assert returning_directories () ==
-
-    def test_returning_lis(self):
-        assert returning_lis () ==
+    # def test_returning_directories(self):
+    #     assert returning_directories () ==
+    #
+    # def test_returning_lis(self):
+    #     assert returning_lis () ==
